596020d7b9c8d67090c40852d684327a46cec8fa
kishal46 <kishal57j@gmail.com>
2024-06-11 07:56:54 +0530

    Last

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const app = express()
-const user = require('./routes/user')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const user = require('./routes/user');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
+app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/Signup')
+const mongoURI = 'mongodb+srv://kishal:kishal4657@cluster0.0kqtcda.mongodb.net/Kidshop?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connection.on('connected', () => {
-    console.log("MongoDB connected")
-})
+mongoose.connect(mongoURI);
 
-app.use('/login', user)
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+app.use('/login', user);
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
