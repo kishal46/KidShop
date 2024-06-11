8ecbafff2d6513f0c9d09aa85ee8552c1d05b658
kishal46 <kishal57j@gmail.com>
2024-06-11 06:15:47 +0530

    Finesh

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,22 @@
-const express=require('express')
-const app=express()
-const user=require('./routes/user')
-const mongoose=require('mongoose')
-const cors=require('cors')
+const express = require('express')
+const app = express()
+const user = require('./routes/user')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors())
+
 mongoose.connect('mongodb://localhost:27017/Signup')
 
-mongoose.connection.on('connected',()=>{
-    console.log("MongoDB connencted")
+mongoose.connection.on('connected', () => {
+    console.log("MongoDB connected")
 })
-app.use('/login',user)
 
-app.listen(3001,()=>{
-    console.log("Server is running 3001");
+app.use('/login', user)
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
